dc50fd4e849fcef050aadec5bf7ea7107b0faa32
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-30 21:22:17 +0200

    Updated to 0.9.0

--- korte-jekyll/src/com/soywiz/korte/jekyll/KorteJekyll.kt
+++ korte-jekyll/src/com/soywiz/korte/jekyll/KorteJekyll.kt
@@ -1,11 +1,12 @@
 package com.soywiz.korte.jekyll
 
+import com.soywiz.korio.async.AsyncQueue
 import com.soywiz.korio.async.EventLoop
 import com.soywiz.korio.async.WorkQueue
 import com.soywiz.korio.vfs.LocalVfs
 
 object KorteJekyll {
-	val workQueue = WorkQueue()
+	val workQueue = AsyncQueue()
 	val base = LocalVfs("c:/projects/jekylltest")
 	val inp = base["input"]
 	val out = base["output"]
@@ -27,9 +28,10 @@ object KorteJekyll {
 		//}
 	}
 
-	@JvmStatic fun main(args: Array<String>) = EventLoop.main {
+	@JvmStatic fun main(args: Array<String>) = EventLoop {
+		val eventLoop = this
 		inp.watch {
-			workQueue { rebuild() }
+			rebuild()
 		}
 
 		workQueue { rebuild() }
--- korte-vertx/src/com/soywiz/korte/KorteVertxTemplateEngine.kt
+++ korte-vertx/src/com/soywiz/korte/KorteVertxTemplateEngine.kt
@@ -1,6 +1,7 @@
 package com.soywiz.korte
 
 import com.soywiz.korio.async.spawnAndForget
+import com.soywiz.korio.coroutine.CoroutineContext
 import com.soywiz.korio.vertx.vxResult
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
@@ -8,9 +9,9 @@ import io.vertx.core.buffer.Buffer
 import io.vertx.ext.web.RoutingContext
 import io.vertx.ext.web.templ.TemplateEngine
 
-class KorteVertxTemplateEngine(val templates: Templates) : TemplateEngine {
+class KorteVertxTemplateEngine(val coroutineContext: CoroutineContext, val templates: Templates) : TemplateEngine {
 	override fun render(context: RoutingContext, templateFileName: String, handler: Handler<AsyncResult<Buffer>>): Unit {
-		spawnAndForget {
+		spawnAndForget(coroutineContext) {
 			val result = vxResult {
 				val str = templates.get(templateFileName).invoke()
 				Buffer.buffer(str)
