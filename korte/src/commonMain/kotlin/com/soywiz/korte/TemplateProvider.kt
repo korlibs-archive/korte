d0ae20ca56bb2138ee2c8d773e18536a0792b0e5
soywiz <soywiz@gmail.com>
2020-04-13 04:36:29 +0200

    Some updates

--- korte/src/commonMain/kotlin/com/soywiz/korte/TemplateProvider.kt
+++ korte/src/commonMain/kotlin/com/soywiz/korte/TemplateProvider.kt
@@ -1,12 +1,13 @@
 package com.soywiz.korte
 
 interface TemplateProvider {
-	class NotFoundException(val template: String) : RuntimeException()
+	class NotFoundException(val template: String) : RuntimeException("Can't find template '$template'")
 
 	suspend fun get(template: String): String?
 }
 
-suspend fun TemplateProvider.getSure(template: String) = get(template) ?: throw TemplateProvider.NotFoundException(template)
+suspend fun TemplateProvider.getSure(template: String) = get(template)
+    ?: throw TemplateProvider.NotFoundException(template)
 
 fun TemplateProvider(map: Map<String, String>): TemplateProvider = object : TemplateProvider {
 	override suspend fun get(template: String): String? = map[template]
